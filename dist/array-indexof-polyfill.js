0413f11b307f87614756e5adf20cdd6220ff8cd0
Matt Toledo <mtoledo@resource.com>
2014-04-11 16:35:43 -0400

    uglyfy was complaining of a missing semicolon.

--- dist/array-indexof-polyfill.js
+++ dist/array-indexof-polyfill.js
@@ -14,9 +14,9 @@
   ) { 
      for (
         // initialize length
-        var c = this.length,
+        var c = this.length;
             // initialize counter (allow for negative startIndex)
-            b = (c + ~~b) % c
+        var b = (c + ~~b) % c;
         // loop if index is smaller than length,
         // index is set in (possibly sparse) array
         // and item at index is not identical to the searched one
@@ -26,5 +26,5 @@
      );
      // if counter equals length (not found), return -1, otherwise counter
      return b ^ c ? b : -1;
-  })
+  });
 })();
