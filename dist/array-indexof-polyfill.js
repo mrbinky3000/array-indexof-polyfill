ade6d2adb77469ae41c29eec1c51605cef615785
Matt Toledo <mtoledo@resource.com>
2014-04-11 16:05:35 -0400

    found a better polyfill to Bower-fy

--- dist/array-indexof-polyfill.js
+++ dist/array-indexof-polyfill.js
@@ -1,31 +1,30 @@
 /**
  * Array indexOf() polyfill for ie < 9. Packaged for Bower by
- * Matthew Toledo.  Original author was Julien Bouquillon.
+ * Matthew Toledo.  Original author was https://gist.github.com/atk/1034425
  * 
- * @see https://gist.github.com/revolunet/1908355
- * @author Julien Bouquillon
- * @return void
+ * @see https://gist.github.com/atk/1034425
  */
 (function() {
-if (!Array.prototype.indexOf)
-  {
-    Array.prototype.indexOf = function(elt /*, from*/)
-    {
-      var len = this.length >>> 0;
-      var from = Number(arguments[1]) || 0;
-      from = (from < 0)
-           ? Math.ceil(from)
-           : Math.floor(from);
-      if (from < 0)
-        from += len;
-   
-      for (; from < len; from++)
-      {
-        if (from in this &&
-            this[from] === elt)
-          return from;
-      }
-      return -1;
-    };
-  }
+// Avoid overwriting prototype if not neccessary
+  [].indexOf||(Array.prototype.indexOf=
+  function(
+     a, // search item
+     b, // startIndex and/or counter
+     c  // length placeholder
+  ) { 
+     for (
+        // initialize length
+        var c = this.length,
+            // initialize counter (allow for negative startIndex)
+            b = (c + ~~b) % c
+        // loop if index is smaller than length,
+        // index is set in (possibly sparse) array
+        // and item at index is not identical to the searched one
+        b < c && (!(b in this || this[b] !== a));
+        // increment counter
+        b++
+     );
+     // if counter equals length (not found), return -1, otherwise counter
+     return b ^ c ? b : -1;
+  })
 })();
