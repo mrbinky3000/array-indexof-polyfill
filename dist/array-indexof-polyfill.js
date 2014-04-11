259fbb3052c638cdaf9c42042e4eff4e1ca08c14
Matt Toledo <mtoledo@resource.com>
2014-04-11 16:46:49 -0400

    GAHHHH!!!!!!!

--- dist/array-indexof-polyfill.js
+++ dist/array-indexof-polyfill.js
@@ -6,25 +6,8 @@
  */
 (function() {
 // Avoid overwriting prototype if not neccessary
-  [].indexOf||(Array.prototype.indexOf=
-  function(
-     a, // search item
-     b, // startIndex and/or counter
-     c  // length placeholder
-  ) { 
-     for (
-        // initialize length
-        var c = this.length;
-            // initialize counter (allow for negative startIndex)
-        var b = (c + ~~b) % c;
-        // loop if index is smaller than length,
-        // index is set in (possibly sparse) array
-        // and item at index is not identical to the searched one
-        b < c && (!(b in this || this[b] !== a));
-        // increment counter
-        b++
-     );
-     // if counter equals length (not found), return -1, otherwise counter
+  [].indexOf||(Array.prototype.indexOf=function(a,b,c) {
+     for ( var c = this.length, b = (c + ~~b) % c; b < c && (!(b in this || this[b] !== a)); b++){};
      return b ^ c ? b : -1;
   });
 })();
