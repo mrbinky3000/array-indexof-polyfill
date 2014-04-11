c273034c34c343430827f4b0de9bf1d7b1d36a26
Matt Toledo <mtoledo@resource.com>
2014-04-11 12:56:24 -0400

    Initial commit

---
+++ array-indexof-polyfill.js
@@ -0,0 +1,30 @@
+/**
+ * Array indexOf() polyfill for ie < 9.
+ * 
+ * @see https://gist.github.com/revolunet/1908355
+ * @author Julien Bouquillon
+ * @return void
+ */
+(function() {
+if (!Array.prototype.indexOf)
+  {
+    Array.prototype.indexOf = function(elt /*, from*/)
+    {
+      var len = this.length >>> 0;
+      var from = Number(arguments[1]) || 0;
+      from = (from < 0)
+           ? Math.ceil(from)
+           : Math.floor(from);
+      if (from < 0)
+        from += len;
+   
+      for (; from < len; from++)
+      {
+        if (from in this &&
+            this[from] === elt)
+          return from;
+      }
+      return -1;
+    };
+  }
+})();
